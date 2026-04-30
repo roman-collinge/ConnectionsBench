ae6d3bb08045f3441998318654efbcce26fc42b5
roman-collinge <romancollinge@outlook.com>
2026-04-30 21:12:57 +0100

    feat(results): stub results functions

---
+++ src/connectionsbench/results.py
@@ -0,0 +1,31 @@
+"""
+Results loading and leaderboard calculation for ConnectionsBench.
+"""
+
+from pathlib import Path
+
+_DEFAULT_RESULTS_DIR = Path(__file__).parent.parent.parent / "results"
+
+
+def load_results():
+    pass
+
+
+def save_result():
+    pass
+
+
+def get_run_puzzle_ids():
+    pass
+
+
+def check_duplicate_run():
+    pass
+
+
+def calculate_leaderboard():
+    pass
+
+
+def calculate_model_metrics():
+    pass
